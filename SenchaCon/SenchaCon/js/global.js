c9b42371a3d45ef871f0f89c33ab0089cbe67d0f
Clark Sell <Clark.Sell@csell.net>
2013-06-11 16:56:29 -0500

    updated datarequest for sharing to include the image as a item in the request

--- SenchaCon/SenchaCon/js/global.js
+++ SenchaCon/SenchaCon/js/global.js
@@ -1,57 +1,54 @@
-﻿var shareDataHandler = function (e) {
-    console.log('shareDataHandler...');
+﻿(function () {
+    "use strict";
 
-    var request = e.request;
-    var dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
-    var deferral = request.getDeferral();
-
-    // create an html fragment
-    var safeHtml = Windows.ApplicationModel.DataTransfer.HtmlFormatHelper.createHtmlFormat(
-        "<div><img src='shareImage.png' /></div>"
-    );
-
-    var imgData = Windows.Security.Cryptography.CryptographicBuffer.decodeFromBase64String(
-        getImageDataFromCanvas()
-    );
-
-    var memoryStream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
-    var dataWriter = new Windows.Storage.Streams.DataWriter(memoryStream);
-    dataWriter.writeBuffer(imgData);
-
-    dataWriter.storeAsync().done(function () {
-        console.log('storeAsync...');
-
-        dataWriter.flushAsync().done(function () {
-            console.log('flushAsync...');
-
-            var imgStream = dataWriter.detachStream();
-            imgStream.seek(0);
-            var streamReference = Windows.Storage.Streams.RandomAccessStreamReference.createFromStream(imgStream);
-
-            dataPackage.resourceMap["shareImage.png"] = streamReference;
-
-            dataPackage.setHtmlFormat(safeHtml);
-
-            request.data = dataPackage;
-            request.data.properties.title = "SenchaCon wants to share...";
-            request.data.properties.description = "Art's really cool SenchaCon session!";
-
-            deferral.complete();
-        });
+    var page = WinJS.UI.Pages.define("/default.html", {
+        ready: function (element, options) {
+            console.log('page control ready, loading dataTransfer object...');
+            var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+            dataTransferManager.addEventListener("datarequested", dataRequested);
+        },
+        unload: function () {
+            var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+            dataTransferManager.removeEventListener("datarequested", dataRequested);
+        }
     });
 
-};
+    var getImageDataFromCanvas = function () {
+        console.log('getImageDataFromCanvas');
 
-var getImageDataFromCanvas = function () {
-    console.log('getImageDataFromCanvas');
+        var canvas = document.getElementsByTagName("canvas")[0];
+        var img = canvas.toDataURL("image/png");
+        return img.substr(22);
+    };
 
-    var canvas = document.getElementsByTagName("canvas")[0];
-    var img = canvas.toDataURL("image/png");      // Get the data as an image.
-    return img.substr(22);
-};
+    function dataRequested(e) {
+        console.log('in dataRequested...');
 
-if (Windows.ApplicationModel) {
-    console.log('Setting datarequested event handler...');
-    var dtm = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
-    dtm.addEventListener("datarequested", shareDataHandler);
-}
+        var request = e.request;
+        var deferral = request.getDeferral();
+
+        var imgData = Windows.Security.Cryptography.CryptographicBuffer.decodeFromBase64String(
+            getImageDataFromCanvas()
+        );
+
+        Windows.Storage.KnownFolders.picturesLibrary.createFileAsync("tranfer.png", Windows.Storage.CreationCollisionOption.replaceExisting).done(
+            function (file) {
+
+                Windows.Storage.FileIO.writeBufferAsync(file, imgData).done(function () {
+
+                    var streamReference = Windows.Storage.Streams.RandomAccessStreamReference.createFromFile(file);
+
+                    request.data.properties.title = "SenchaCon wants to share...";
+                    request.data.properties.description = "Art's really cool SenchaCon session!";
+                    request.data.properties.thumbnail = streamReference;
+
+                    request.data.setStorageItems([file]);
+                    request.data.setBitmap(streamReference);
+
+                    deferral.complete();
+                });
+            }
+        );
+    };
+
+})();
